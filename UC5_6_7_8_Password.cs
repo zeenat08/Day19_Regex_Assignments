4abd8536b9963e0f91cd0211b82faee8bc9cc5b4
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-05 23:03:24 +0530

    ZEENAT:[added] Regex pattern for checking user's password

---
+++ UC5_6_7_8_Password.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex_Assignments
+{
+    class UC5_6_7_8_Password
+    {
+        public static string Regex_PSWD = "^([a-zA-Z0-9]*[A-Z]{1,}[0-9]{1,}[$#.%+@-~!]{1}){8,}$";
+
+            public bool ValidatePassword(string password)
+            {
+                return Regex.IsMatch(password, Regex_PSWD);
+            }
+    }
+}

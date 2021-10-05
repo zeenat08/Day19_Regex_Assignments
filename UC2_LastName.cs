7d5b9d3e405ed379f96f1d55444970e481a583f5
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-05 22:56:06 +0530

    ZEENAT:[added] Regex pattern for Last name

---
+++ UC2_LastName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex_Assignments
+{
+    class UC2_LastName
+    {
+
+        public static string Regex_Lastname = "^[A-Z]{1}[a-z]{2,}$";
+
+        public bool ValidateLastName(string lastname)
+        {
+            return Regex.IsMatch(lastname, Regex_Lastname);
+        }
+    }
+}

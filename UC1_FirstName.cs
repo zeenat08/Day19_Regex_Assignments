e4b19203c9af807f14afee572d75788081ba581a
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-05 22:50:39 +0530

    ZEENAT:[added] Regex pattern for first name

---
+++ UC1_FirstName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex_Assignments
+{
+    class UC1_FirstName
+    {
+        public static string Regex_Firstname = "^[A-Z]{1}[a-z]{2,}$";
+
+        public bool ValidateFirstName(string firstname)
+        {
+            return Regex.IsMatch(firstname, Regex_Firstname);
+        }
+    }
+}

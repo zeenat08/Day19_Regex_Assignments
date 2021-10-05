ffeee3871570ed63a29ba7622945df259a11a1b7
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-05 23:01:22 +0530

    ZEENAT:[added] Regex pattern for checking mobile number

---
+++ UC4_Mobile_No.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex_Assignments
+{
+    class UC4_Mobile_No
+    {
+        public static string Regex_MobileNumber = "^[0-9]{2}[ ][0-9]{10}$";
+
+        public bool ValidatePhoneNumber(string phonenumber)
+        {
+            return Regex.IsMatch(phonenumber, Regex_MobileNumber);
+        }
+    }
+}

3af73e47756f84a5938cca323db54128d508ee21
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-05 22:59:05 +0530

    ZEENAT:[added] Regex pattern for checking email id

---
+++ UC3_9_Email_Id.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex_Assignments
+{
+    class UC3_9_Email_Id
+    {
+        //^[a-zA-Z0-9]+ --> Starts with Letters and Numbers(At least one char is required)
+        //([.#_$+-][a-zA-Z0-9]+)* -->for 1st optional part
+        //[@] --> Followed by @
+        //[a-zA-Z0-9]+ --> should have Alphabets and Numbers(At least one char is required)
+        //[.] --> followed by .
+        //[a-zA-Z]{2-3] --> only aphabets and no numbers and max 3
+        //([.][a-zA-z0-9]{2})? --> for 2nd optional part
+
+        public static string Regex_Email = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+
+        public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, Regex_Email);
+        }
+    }
+}
